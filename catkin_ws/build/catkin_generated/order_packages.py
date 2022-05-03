c3793653ff40b5af8cb5baa85a0c9f630f931651
kieranreillyuwa <22484032@student.uwa.edu.au>
2022-05-03 02:41:33 -0400

    transfer

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/netipc/RobotsAndCones/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/netipc/ins_ws/devel;/opt/ros/noetic'.split(';') if '/home/netipc/ins_ws/devel;/opt/ros/noetic' != '' else []
