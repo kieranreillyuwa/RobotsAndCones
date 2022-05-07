70f10003f813cb08ace921b9b8a40c65a5d444fd
kieranreillyuwa <22484032@student.uwa.edu.au>
2022-05-07 06:30:51 -0400

    bakup

--- catkin_ws/src/NaughtyRobot/src/main_node.cpp
+++ catkin_ws/src/NaughtyRobot/src/main_node.cpp
@@ -2,23 +2,56 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/MagneticField.h>
 
 uint8_t i = 0;
+
+sensor_msgs::NavSatFix prevPos;
 
 void GpsCallBack(const sensor_msgs::NavSatFixConstPtr& msg)
 {
     ROS_INFO("Latitude %d: %.9f",i,msg->latitude);
     ROS_INFO("Longitude %d: %.9f",i++,msg->longitude);
+
+	float heading = atan2(msg->latitude-prevPos.latitude,msg->longitude-prevPos.longitude)*180/M_PI;
+	ROS_INFO("Heading from GPS: %.9f",heading*180/M_PI);
+	prevPos = *msg;
 }
+
+void ImuCallBack(const sensor_msgs::MagneticFieldConstPtr& msg)
+{
+    float heading = atan2(msg->magnetic_field.y,msg->magnetic_field.x);
+    ROS_INFO("Heading: %.9f", heading*180/M_PI);
+}
+
+/**
+ * @brief Blocking rotation
+ * 
+ * @param angle the angle in radians
+ * @param speed speed in rads^-1
+ */
+void Rotate(float angle, ros::Publisher *pcmdVelPub, float speed = 0.5)
+{
+
+    ros::Rate rate(speed/angle);
+    geometry_msgs::Twist vel;
+    vel.linear.x = 0;
+    vel.angular.z = speed;
+    pcmdVelPub->publish(vel);
+    rate.sleep();
+    vel.angular.z = 0;
+    pcmdVelPub->publish(vel);
+}
+
+
 
 int main(int argc, char** argv)
 {
      ros::init(argc, argv, "Main_Node");
     ros::NodeHandle nh;
-	//ros::Rate rate(20);
-
-   ros::Publisher cmdVelPub = nh.advertise<geometry_msgs::Twist>("RosAria/cmd_vel",10);
+ros::Publisher cmdVelPub = nh.advertise<geometry_msgs::Twist>("RosAria/cmd_vel",10);
     ros::Subscriber gpsSub = nh.subscribe<sensor_msgs::NavSatFix>("fix",10,GpsCallBack);
+    ros::Subscriber imuMagSub = nh.subscribe<sensor_msgs::MagneticField>("imu/mag",10,ImuCallBack);
 
     geometry_msgs::Twist vel;
     vel.linear.x = 1.0;
@@ -29,67 +62,70 @@ int main(int argc, char** argv)
 
     ros::Rate rate(10);
 
-    while(ros::ok())
-    {
+	prevPos.latitude = 0;
+	prevPos.longitude = 0;
 
-        switch(state)
-        {
-            case 0:
-                if(ros::Time::now()-startTime > ros::Duration(5.0))
-                {
-                    state = 1;
-                    vel.linear.x = 0.0;
-                    vel.angular.z = 0.0;
-                    cmdVelPub.publish(vel);
-                    startTime = ros::Time::now();
-                    break;
-                }
-                else
-                {
-                    vel.linear.x = 0.5;
-                vel.angular.z = 0.0;
-                cmdVelPub.publish(vel);
-                }break;
-            case 1:
-                if(ros::Time::now()-startTime > ros::Duration(5.0))
-                {
-                    state = 2;
-                    vel.linear.x = 0.0;
-                    vel.angular.z = 0.0;
-                    cmdVelPub.publish(vel);
-                    startTime = ros::Time::now();
-                    break;
-                }
-                else
-                {
-                    vel.linear.x = 0.5;
-                    vel.angular.z = -0.1;
-                    cmdVelPub.publish(vel);
-                }
-                break;
-            case 2:
-                if(ros::Time::now()-startTime > ros::Duration(5.0))
-                {
-                    state = 3;
-                    vel.linear.x = 0.0;
-                    vel.angular.z = 0.0;
-                    cmdVelPub.publish(vel);
-                    return 0;
-                }
-                else
-                {
-                    vel.linear.x = 0.5;
-                    vel.angular.z = 0;
-                    cmdVelPub.publish(vel);
-                }
-                break;
+    // while(ros::ok())
+    // {
+
+        // switch(state)
+        // {
+        //     case 0:
+        //         if(ros::Time::now()-startTime > ros::Duration(5.0))
+        //         {
+        //             state = 1;
+        //             vel.linear.x = 0.0;
+        //             vel.angular.z = 0.0;
+        //             cmdVelPub.publish(vel);
+        //             startTime = ros::Time::now();
+        //             break;
+        //         }
+        //         else
+        //         {
+        //             vel.linear.x = 0.5;
+        //         vel.angular.z = 0.0;
+        //         cmdVelPub.publish(vel);
+        //         }break;
+        //     case 1:
+        //         if(ros::Time::now()-startTime > ros::Duration(5.0))
+        //         {
+        //             state = 2;
+        //             vel.linear.x = 0.0;
+        //             vel.angular.z = 0.0;
+        //             cmdVelPub.publish(vel);
+        //             startTime = ros::Time::now();
+        //             break;
+        //         }
+        //         else
+        //         {
+        //             vel.linear.x = 0.5;
+        //             vel.angular.z = -0.1;
+        //             cmdVelPub.publish(vel);
+        //         }
+        //         break;
+        //     case 2:
+        //         if(ros::Time::now()-startTime > ros::Duration(5.0))
+        //         {
+        //             state = 3;
+        //             vel.linear.x = 0.0;
+        //             vel.angular.z = 0.0;
+        //             cmdVelPub.publish(vel);
+        //             // return 0;
+        //         }
+        //         else
+        //         {
+        //             vel.linear.x = 0.5;
+        //             vel.angular.z = 0;
+        //             cmdVelPub.publish(vel);
+        //         }
+        //         break;
                 
-        }
+        Rotate(M_PI,&cmdVelPub);
 
         ros::spinOnce();
         rate.sleep();
 
-    }
+    
 
 
     return 0;
